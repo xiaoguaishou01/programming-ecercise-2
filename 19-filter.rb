b2b2524c51a7ec5c5496c900f8007005a962426a
韦月炎 <weiyueyan@MBP.local>
2017-08-29 22:43:20 +0800

    19-filter.rb

--- 19-filter.rb
+++ 19-filter.rb
@@ -1,9 +1,14 @@
 # 给定一阵列内含数字，输出另一个数组只包含偶数
 
 def filter_even(arr)
-  #...
+  array = []
+  for i in arr
+    if i % 2 == 0
+    array << i
+    end
+  end
+  return array
 end
-
 arr =  [7, 68, 42, 46, 9, 91, 77, 46, 86, 1]
 
 puts filter_even(arr).to_s # 应该是 [68, 42, 46, 46, 86]
